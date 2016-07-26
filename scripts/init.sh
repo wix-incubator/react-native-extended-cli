c1ce7c8469c052118892192f60fd2ccc22072015
Aaron Greenwald <aarong@wix.com>
2016-07-26 19:18:27 +0300

    add babel env for specs

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,13 @@
 projectName=$1
-echo Initialling React Native project $projectName with version: $2
+rnVersion=$2
 
-rninit init $projectName --source react-native@$2
+if [ "$rnVersion" == "" ]; then
+    rnVersion="@latest"
+fi
+
+echo Initialling React Native project $projectName with version: $rnVersion
+
+rninit init $projectName --source react-native@$rnVersion
 
 rnxRoot=${BASH_SOURCE[0]%/*}/../
 
@@ -9,27 +15,15 @@ cd $projectName
 
 cp -r $rnxRoot/template/ ./ 
 
-jq -r '.version="1.0.0" \
-    | .main="src/module.js" \
-    | .scripts.build="rnx build" \
-    | .scripts.release="rnx release" \
-    | .scripts.lint="eslint test src" \
-    | .scripts.test="rnx test" \
-    | .config.appName="$projectName" \
+jq -r '.version="1.0.0"
+    | .main="src/module.js"
+    | .scripts.build="rnx build"
+    | .scripts.release="rnx release"
+    | .scripts.lint="eslint test src"
+    | .scripts.test="rnx test"
+    | .babel.env.specs.presets=["es2015", "react", "stage-0"]
+    | .babel.env.specs.retainLines="true"
+    | .config.appName="$projectName"    
     | .config.iphoneModel="iPhone 6s" ' package.json > tmp.json && mv tmp.json package.json
 
 npm install react-native-extended-cli enzyme proxyquire react-dom react-native-mock --save-dev
-
-#TODO: Add this to package.json
-# "babel": {
-#     "env": {
-#       "specs": {
-#         "presets": [
-#           "es2015",
-#           "react",
-#           "stage-0"
-#         ],
-#         "retainLines": true
-#       }
-#     }
-#   }
