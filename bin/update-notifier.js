3336284b6e17e40748ed2233537f87069d288148
Aaron Greenwald <aarong@wix.com>
2016-07-26 12:58:48 +0300

    rninit

--- bin/update-notifier.js
+++ bin/update-notifier.js
@@ -3,4 +3,3 @@ const pkg = require('../package.json');
 const updateCheckInterval = 1000; //once we stop iterating super fast this could be lengthened by a bunch. like once a day, maybe once a week
 const notifier = updateNotifier({pkg, updateCheckInterval});
 notifier.notify();
-
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-react-native init
+rninit init $1 --source react-native@$2
 
 touch demo-app.component.js
 mkdir src
