3e72b2d132775e31210070efe0a62d821528e2ba
Aaron Greenwald <aarong@wix.com>
2016-07-28 11:23:01 +0300

    finally - better teamcity feedback

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,11 @@
 
 echo "##teamcity[blockOpened name='rnx build']"
 
-echo "##teamcity[buildStatus status='Lint Failure' text='Lint Failure.']"
-exit 3
-
 echo "##teamcity[blockOpened name='rnx lint']"
 $rnxRoot/scripts/lint.sh
 if [ $? -ne 0 ]; then
     echo "Lint failed: build terminating"
-    echo "##teamcity[buildStatus status='Lint Failure' text='Lint Failure.']"
+    echo "##teamcity[buildStatus status='Lint Failure' text='Lint Failure']"
     exit 3
 fi
 echo "##teamcity[blockClosed name='rnx lint']"
@@ -17,7 +14,8 @@ echo "##teamcity[blockClosed name='rnx lint']"
 echo "##teamcity[blockOpened name='rnx test unit']"
 $rnxRoot/scripts/test.sh unit
 if [ $? -ne 0 ]; then
-    echo "Tests failed: build terminating"    
+    echo "Tests failed: build terminating" 
+    echo "##teamcity[buildStatus status='Unit Test Failure' text='Unit Test Failure']"   
     exit 4
 fi
 echo "##teamcity[blockClosed name='rnx test unit']"
@@ -26,6 +24,7 @@ echo "##teamcity[blockOpened name='iOS Build']"
 $rnxRoot/util/build.ios.sh $1
 if [ $? -ne 0 ]; then
     echo "iOS build failed"    
+    echo "##teamcity[buildStatus status='iOS Build Failed' text='iOS Build Failed']"
     exit 5
 fi
 echo "##teamcity[blockClosed name='iOS Build']"
@@ -34,6 +33,7 @@ echo "##teamcity[blockOpened name='Android Build']"
 $rnxRoot/util/build.android.sh $1
 if [ $? -ne 0 ]; then
     echo "Android build failed"    
+    echo "##teamcity[buildStatus status='Android Build Failed' text='Android Build Failed']"
     exit 6
 fi
 echo "##teamcity[blockClosed name='Android Build']"
