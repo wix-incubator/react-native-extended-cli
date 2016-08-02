da8ecce0898be42004c16089c36b3e5739ec22ce
Aaron Greenwald <aarong@wix.com>
2016-08-02 13:30:35 +0300

    fix logger bug

--- util/logger.sh
+++ util/logger.sh
@@ -2,6 +2,6 @@ if [ "${IS_BUILD_AGENT}" == true ]; then
     if [ "$1" == "buildStatus" ]; then
         echo "##teamcity[buildStatus status='$2' text='$2']"
     else
-        echo "##teamcity[$1 name=$2]"
+        echo "##teamcity[$1 'name=$2']"
     fi
 fi
