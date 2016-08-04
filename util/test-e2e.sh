5f5e177c11fe1ca2f43a8d596556cd92e1947dad
Aaron Greenwald <aarong@wix.com>
2016-08-04 18:04:09 +0300

    run fake-server in CI for detox as well

--- util/test-e2e.sh
+++ util/test-e2e.sh
@@ -3,20 +3,18 @@
 $rnxRoot/util/logger.sh blockOpened E2E 
 set +e
 
+if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then  
+  $rnxRoot/util/killProcess.sh fake-server # kill other fake servers in the CI to clear port 3000
+  nohup npm run fake-server &
+else
+  # disconnect hardware keyboard from simulator
+  osascript $rnxRoot/util/set_hardware_keyboard.applescript 0
+fi
+
 #todo deprecate jasmine/appium support eventually
 if [ -f "./test/e2e/support/jasmine-runner.js" ]; then
-  $rnxRoot/util/killProcess.sh appium
-
-  if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then  
-    $rnxRoot/util/killProcess.sh fake-server # kill other fake servers in the CI to clear port 3000
-    nohup npm run fake-server &
-  else
-    # disconnect hardware keyboard from simulator
-    osascript $rnxRoot/util/set_hardware_keyboard.applescript 0
-  fi
-
+  $rnxRoot/util/killProcess.sh appium  
   BABEL_ENV=specs specFilterString=./test/e2e/*.e2e.spec.js node ./test/e2e/support/jasmine-runner.js
-
 else 
   $rnxRoot/util/killProcess.sh detox-server
   ./node_modules/.bin/detox-server &
