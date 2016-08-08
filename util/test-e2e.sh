ae8ab394af103ac1ad0ff1923823b73d6ce5bb14
Aaron Greenwald <aarong@wix.com>
2016-08-08 10:42:39 +0300

    write fake-server logs to output in CI

--- util/test-e2e.sh
+++ util/test-e2e.sh
@@ -5,7 +5,7 @@ set +e
 
 if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then  
   $rnxRoot/util/killProcess.sh fake-server # kill other fake servers in the CI to clear port 3000
-  nohup npm run fake-server &
+  npm run fake-server &
 else
   # disconnect hardware keyboard from simulator
   osascript $rnxRoot/util/set_hardware_keyboard.applescript 0
