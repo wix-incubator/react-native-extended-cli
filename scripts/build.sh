a0cfde6a899f7427c333268ad02529e0dfe593c8
Aaron Greenwald <aarong@wix.com>
2016-08-08 11:33:25 +0300

    support force in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 $rnxRoot/util/logger.sh blockOpened "rnx build"
 $rnxRoot/util/logger.sh blockOpened "rnx lint"
 $rnxRoot/scripts/lint.sh
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 && $* != *--force* ]]; then
     echo "Lint failed: build terminating"
     $rnxRoot/util/logger.sh buildStatus "Lint Failed"
     exit 3
@@ -12,7 +12,7 @@ $rnxRoot/util/logger.sh blockClosed "rnx lint"
 
 $rnxRoot/util/logger.sh blockOpened "rnx test unit"
 $rnxRoot/scripts/test.sh unit
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 && $* != *--force* ]]; then
     echo "Tests failed: build terminating" 
     $rnxRoot/util/logger.sh buildStatus "Unit Tests Failed"
     exit 4
@@ -22,7 +22,7 @@ $rnxRoot/util/logger.sh blockClosed "rnx test unit"
 if [[ $* != *--skip-ios* ]]; then
     $rnxRoot/util/logger.sh blockOpened "iOS Build"
     $rnxRoot/util/build.ios.sh $1
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 && $* != *--force* ]]; then
         echo "iOS build failed"    
         $rnxRoot/util/logger.sh buildStatus "iOS Build Failed"
         exit 5
@@ -33,7 +33,7 @@ fi
 if [[ $* != *--skip-android* ]]; then
     $rnxRoot/util/logger.sh blockOpened "Android Build"
     $rnxRoot/util/build.android.sh $1
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 && $* != *--force* ]]; then
         echo "Android build failed"    
         $rnxRoot/util/logger.sh buildStatus "Android Build Failed"
         exit 6
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 echo Linting...
 echo 
 eslint src test
