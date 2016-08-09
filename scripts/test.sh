21a1a707fca709bfc78f15f6062d66067f6a7e41
Doron Pearl <doronpe@wix.com>
2016-08-09 16:40:47 -0400

    send additional (optional) params to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" != "e2e" ]; then
   $rnxRoot/util/logger.sh blockOpened "Unit Tests"
-  $rnxRoot/util/test-unit.sh
+  $rnxRoot/util/test-unit.sh $@
   if [ $? -ne 0 ]; then
     echo "Unit Tests failed"    
     $rnxRoot/util/logger.sh buildStatus "Unit Tests Failed"
@@ -14,7 +14,7 @@ fi
 
 if [ "$1" != "unit" ]; then
   $rnxRoot/util/logger.sh blockOpened "E2E Tests"
-  $rnxRoot/util/test-e2e.sh $1
+  $rnxRoot/util/test-e2e.sh $@
   if [ $? -ne 0 ]; then
     echo "E2E Tests failed"
     $rnxRoot/util/logger.sh buildStatus "E2E Tests Failed"    
--- util/test-e2e.sh
+++ util/test-e2e.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$rnxRoot/util/logger.sh blockOpened E2E 
+$rnxRoot/util/logger.sh blockOpened E2E
 set +e
 
-if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then  
+if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then
   $rnxRoot/util/killProcess.sh fake-server # kill other fake servers in the CI to clear port 3000
   npm run fake-server &
 else
@@ -13,19 +13,19 @@ fi
 
 #todo deprecate jasmine/appium support eventually
 if [ -f "./test/e2e/support/jasmine-runner.js" ]; then
-  $rnxRoot/util/killProcess.sh appium  
-  BABEL_ENV=specs specFilterString=./test/e2e/*.e2e.spec.js node ./test/e2e/support/jasmine-runner.js
-else 
+  $rnxRoot/util/killProcess.sh appium
+  BABEL_ENV=specs specFilterString=./test/e2e/*.e2e.spec.js node ./test/e2e/support/jasmine-runner.js $@
+else
   $rnxRoot/util/killProcess.sh detox-server
   ./node_modules/.bin/detox-server &
-  BABEL_ENV=specs mocha test/e2e --opts ./test/e2e/mocha.opts
+  BABEL_ENV=specs mocha test/e2e --opts ./test/e2e/mocha.opts $@
 fi
 
 exitCode=$?
 set -e
-$rnxRoot/util/logger.sh blockClosed E2E 
+$rnxRoot/util/logger.sh blockClosed E2E
 
-if [ "${IS_BUILD_AGENT}" == true ]; then  
+if [ "${IS_BUILD_AGENT}" == true ]; then
   $rnxRoot/util/killProcess.sh ./node_modules/react-native/packager/launchPackager.command
   $rnxRoot/util/killProcess.sh "appium"
   $rnxRoot/util/killProcess.sh "Simulator"
