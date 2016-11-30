0ea197f58efbff0c6260638f4613ebe6fbb8f476
Aaron Greenwald <aarong@wix.com>
2016-11-30 18:47:38 +0200

    kill existing process on 8081 when running start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+pid=`lsof -n -i:8081 | grep LISTEN | awk '{ print $2 }' | uniq`
+if [ "$pid" != "" ]; then
+  kill -9 $pid
+fi
 watchman watch-del-all && (adb reverse tcp:8081 tcp:8081 || true) && node node_modules/react-native/local-cli/cli.js start
