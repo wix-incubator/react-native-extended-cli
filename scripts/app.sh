219b084c692c9feae2841fca59105d7340715223
Aaron Greenwald <aarong@wix.com>
2016-11-30 18:26:03 +0200

    support rnx app for android
    not 100% reliable yet, but sometimes works.

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-buildMode="Debug"
-appName=$npm_package_config_appName
-packageName=$npm_package_config_packageName
-appPath=./ios/DerivedData/${appName}/Build/Products/${buildMode}-iphonesimulator/${appName}.app
 
-echo "Starting ${appName} in ${buildMode} mode"
-node "$rnxRoot/util/start-simulator.ios.js" $npm_package_config_iphoneModel
+if [ "$1" != "android" ]; then
 
-# install app
-CURRENT_BUNDLE_IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$appPath/Info.plist")
-xcrun simctl uninstall booted ${CURRENT_BUNDLE_IDENTIFIER}
-xcrun simctl install booted "$appPath"
-xcrun simctl launch booted $packageName
+  buildMode="Debug"
+  appName=$npm_package_config_appName
+  packageName=$npm_package_config_packageName
+  appPath=./ios/DerivedData/${appName}/Build/Products/${buildMode}-iphonesimulator/${appName}.app
+
+  echo "Starting ${appName} in ${buildMode} mode"
+  node "$rnxRoot/util/start-simulator.ios.js" $npm_package_config_iphoneModel
+
+  # install app
+  CURRENT_BUNDLE_IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$appPath/Info.plist")
+  xcrun simctl uninstall booted ${CURRENT_BUNDLE_IDENTIFIER}
+  xcrun simctl install booted "$appPath"
+  xcrun simctl launch booted $packageName
+
+
+fi
+
+if [ "$1" != "ios" ]; then
+  echo "Starting emulator..."
+  $ANDROID_HOME/tools/emulator -avd 'NEXUS_5X_API_23' &
+
+  WAIT_CMD="adb wait-for-device shell getprop init.svc.bootanim"
+  until $WAIT_CMD | grep -m 1 stopped; do
+    printf "."
+    sleep 0.1
+  done
+  echo 'Emulator started...'
+
+  firstDevice=$(adb devices | tail -n +2 | awk '{print $1}')
+  adb -s $firstDevice install -r  android/app/build/outputs/apk/app-debug.apk
+  adb shell monkey -p com.mobile -c android.intent.category.LAUNCHER 1
+
+fi
