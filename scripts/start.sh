b64caed5efb3a5d5dd06eead5170b78ad5c130e2
Aaron Greenwald <aarong@wix.com>
2017-01-11 10:39:19 +0200

    adb reverse 3000 as well as 8081 on start

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,7 @@ pid=`lsof -n -i:8081 | grep LISTEN | awk '{ print $2 }' | uniq`
 if [ "$pid" != "" ]; then
   kill -9 $pid
 fi
-watchman watch-del-all && (adb reverse tcp:8081 tcp:8081 || true) && node node_modules/react-native/local-cli/cli.js start
+watchman watch-del-all
+adb reverse tcp:8081 tcp:8081 || true
+adb reverse tcp:3000 tcp:3000 || true
+node node_modules/react-native/local-cli/cli.js start
