63177ee87349ae473e9ea7f1e3a9e5198279c30a
Doron Pearl <doronpe@wix.com>
2017-02-15 09:55:36 +0200

    specify derivedDataPath

--- util/build.ios.sh
+++ util/build.ios.sh
@@ -26,7 +26,7 @@ cd ios
 $rnxRoot/util/logger.sh blockOpened "XCode Build"
 echo Building ${appName} in with scheme: ${scheme}...
 #xcodebuild -scheme ${scheme} clean
-xcodebuild -scheme ${scheme} build -destination "platform=iOS Simulator,name=${model}"
+xcodebuild -scheme ${scheme} build -destination "platform=iOS Simulator,name=${model}" -derivedDataPath "./Build/Products/"
 $rnxRoot/util/logger.sh blockClosed "XCode Build"
 
 # make sure that the bundle was created successfully
