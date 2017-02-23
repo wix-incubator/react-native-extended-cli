5129cc0c945137d1cd917e915f1b92f5dabcb7eb
Aaron Greenwald <aarong@wix.com>
2017-02-23 15:17:56 +0200

    clean up output of rnx start

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@ pid=`lsof -n -i:8081 | grep LISTEN | awk '{ print $2 }' | uniq`
 if [ "$pid" != "" ]; then
   kill -9 $pid
 fi
-watchman watch-del-all
-adb reverse tcp:8081 tcp:8081 || true
-adb reverse tcp:3000 tcp:3000 || true
+watchman watch-del-all >/dev/null
+adb reverse tcp:8081 tcp:8081 >/dev/null 2>&1
+adb reverse tcp:3000 tcp:3000 >/dev/null 2>&1
 node node_modules/react-native/local-cli/cli.js start
