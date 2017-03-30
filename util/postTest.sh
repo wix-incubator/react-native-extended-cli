fcf1b669338d95469c7eeb04d969188cd34c524c
Aaron Greenwald <aarong@wix.com>
2017-03-30 13:15:40 +0300

    don't fail if the `src/facades/mocks.js` hack isn't used

--- util/postTest.sh
+++ util/postTest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ "${IS_BUILD_AGENT}" == true ] || [ "$1" == 'release' ]; then
-  # overwrite mocks.js as e2e/ is npm ignored and otherwise the packager of the containing object will red screen
-  echo "export default {};" > ./src/facades/mocks.js
+#this is a deprecated hack. We'll remove it when dependencies aren't doing this kind of ugly magic.
+# for now it stays
+if [ -f ./src/facades/mocks.js ]; then
+  if [ "${IS_BUILD_AGENT}" == true ] || [ "$1" == 'release' ]; then
+    # overwrite mocks.js as e2e/ is npm ignored and otherwise the packager of the containing object will red screen
+    echo "export default {};" > ./src/facades/mocks.js
+  fi
 fi
