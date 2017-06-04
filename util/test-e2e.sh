5839bac0972774b443557590f406b8ec4be196fb
Aaron Greenwald <aarong@wix.com>
2017-06-04 17:15:26 +0300

    cannot deprecate detox5 yet, some projects depend on it stil

--- util/test-e2e.sh
+++ util/test-e2e.sh
@@ -21,15 +21,26 @@ if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then
 fi
 
 echo "Running Detox tests..."
-mochaFile="mocha.opts"
-if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then
-  if [ -f ./test/e2e/mocha-ci.opts ]; then
-    mochaFile="mocha-ci.opts"
-  fi
+
+#TODO use this block once everyone is on detox 5
+#mochaFile="mocha.opts"
+#if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then
+#  if [ -f ./test/e2e/mocha-ci.opts ]; then
+#    mochaFile="mocha-ci.opts"
+#  fi
+#fi
+#
+#./node_modules/.bin/detox test --configuration ${config} --runner-config ${mochaFile}
+
+detoxVersion=$(jq -r .devDependencies.detox package.json)
+if [[ ${detoxVersion:0:2} == *"5"* ]]; then
+  ./node_modules/.bin/detox test --configuration ${config}
+else
+  echo "Please upgrade to detox@5.x.x, support for other versions in rnx will be soon deprecated"
+  $rnxRoot/util/killProcess.sh detox-server
+  ./node_modules/.bin/detox-server &
+  BABEL_ENV=specs mocha test/e2e --opts ./test/e2e/mocha.opts $@
 fi
-
-./node_modules/.bin/detox test --configuration ${config} --runner-config ${mochaFile}
-
 
 exitCode=$?
 
