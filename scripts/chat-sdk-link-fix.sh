9046645e2266dd7b98862b997492ee730ed2f75b
Eden Gottlieb <edeng@wix.com>
2017-12-03 18:26:43 +0200

    Update firebase react-native index file

--- scripts/chat-sdk-link-fix.sh
+++ scripts/chat-sdk-link-fix.sh
@@ -3,7 +3,7 @@
 if [ "${IS_BUILD_AGENT}" == true ] || [ "${1}" == "release" ]; then
   echo "."
 else
-  firebaseFile="./node_modules/chat-sdk/node_modules/firebase/firebase-react-native.js"
+  firebaseFile="./node_modules/chat-sdk/node_modules/firebase/index.react-native.js"
   echo "Patching ${firebaseFile}"
   sed -i -e "s/require('react-native')/require('..\\/..\\/..\\/chat-mobile\\/node_modules\\/react-native')/g" ${firebaseFile}
 fi
