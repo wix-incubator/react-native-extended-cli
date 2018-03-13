a11c257db523d3fa0fae1397f0409217482ba443
Doron Pearl <doronpe@wix.com>
2018-03-13 19:39:04 +0200

    adding npm shrinkwrap to teamcity artifacts dir

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 mltci release
 
-mkdir ./artifacts
-npm shrinkwrap
-mv ./npm-shrinkwrap.json ./artifacts
+if [ -d ./.teamcity ]; then
+    echo "Adding npm shrinkwrap to teamcity artifacts"
+    npm shrinkwrap
+    mv ./npm-shrinkwrap.json ./.teamcity
+fi
 
 version=$(jq -r '.version' package.json)
 $rnxRoot/util/logger.sh buildStatus "Version: ${version}; {build.status.text}"
