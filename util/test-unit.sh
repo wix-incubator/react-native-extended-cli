1a1a9a72f77684a18982c13a12eab1bba6d559cb
Doron Pearl <doronpe@wix.com>
2018-05-24 12:59:04 +0300

    added --skip-jest option

--- util/test-unit.sh
+++ util/test-unit.sh
@@ -3,19 +3,21 @@ set +e
 
 hasJestConfig=$(jq -r 'has("jest")' package.json)
 
-if [[ ${hasJestConfig} == true ]]; then
-    JEST_OPTS=""
-    if [[ $* == *--jest-watch* ]]; then
-        JEST_OPTS="$JEST_OPTS --watch"
-    fi
+if [[ $* != *--skip-jest* ]]; then
+  if [[ ${hasJestConfig} == true ]]; then
+      JEST_OPTS=""
+      if [[ $* == *--jest-watch* ]]; then
+          JEST_OPTS="$JEST_OPTS --watch"
+      fi
 
-    [ -z "$TEAMCITY_VERSION" ] || {
-        JEST_OPTS="$JEST_OPTS -i --coverage"
-    }
-    CI=true jest $JEST_OPTS
-    if [ $? -ne 0 ]; then 
-        exit 1
-    fi
+      [ -z "$TEAMCITY_VERSION" ] || {
+          JEST_OPTS="$JEST_OPTS -i --coverage"
+      }
+      CI=true jest $JEST_OPTS
+      if [ $? -ne 0 ]; then
+          exit 1
+      fi
+  fi
 fi
 
 if [ -f ./test/spec/support/jasmine-runner.js ]; then
