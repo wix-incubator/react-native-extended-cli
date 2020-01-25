3a9f2a284f282a4341bc8c3c63a7067b9398ac19
Doron Pearl <doronpe@wix.com>
2020-01-25 20:19:32 +0200

    remove obsolete stuff: dependency-check, update-notifier

--- bin/update-notifier.js
+++
@@ -1,5 +0,0 @@
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
-const updateCheckInterval = 1000; //once we stop iterating super fast this could be lengthened by a bunch. like once a day, maybe once a week
-const notifier = updateNotifier({pkg, updateCheckInterval});
-notifier.notify();
--- scripts/fix-redscreen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-find ./node_modules/react-native/React/Modules/RCTRedBox.m -type f -exec sed -i '' 's/clearColor/colorWithRed:0.8 green:0 blue:0 alpha:1/g' {} \;
